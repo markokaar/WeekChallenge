369d2bb8f906bde3a2d3a2cae5a32331922314d9
Marko K <kaarmarko@gmail.com>
2015-08-07 23:00:07 +0300

    Static files, Bootstrap

--- WeekChallenge/views.py
+++ WeekChallenge/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello")
+    return render(request, 'WeekChallenge/index.html')
