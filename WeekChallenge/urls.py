5b82fba482bd6eaef909fb1be3f3885907df7b73
Marko K <kaarmarko@gmail.com>
2015-08-07 23:46:26 +0300

    Some html - index. In progress: about, contact, add, login, register

--- WeekChallenge/urls.py
+++ WeekChallenge/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^contact/', views.contact, name='contact'),
+    url(r'^add/', views.add, name='add'),
+    url(r'^register/', views.register, name='register'),
+    url(r'^login/', views.login, name='login'),
 ]
--- WeekChallenge/views.py
+++ WeekChallenge/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'WeekChallenge/index.html')
+
+
+def about(request):
+    return render(request, 'WeekChallenge/about.html')
+
+
+def contact(request):
+    return render(request, 'WeekChallenge/contact.html')
+
+
+def add(request):
+    return render(request, 'WeekChallenge/add.html')
+
+
+def register(request):
+    return render(request, 'WeekChallenge/register.html')
+
+
+def login(request):
+    return render(request, 'WeekChallenge/login.html')
+
