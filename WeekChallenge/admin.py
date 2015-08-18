4c5951496c29271c350b94c4083fa24bbdc103e3
Marko K <kaarmarko@gmail.com>
2015-08-18 10:39:38 +0300

    Models for messages, notifications, friends. And some bugfixes.

--- WeekChallenge/admin.py
+++ WeekChallenge/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Challenge, UserChallenge
+from .models import Challenge, UserChallenge, UserFriend, Notification, Message
 
 
 admin.site.register(Challenge)
 admin.site.register(UserChallenge)
+admin.site.register(UserFriend)
+admin.site.register(Notification)
+admin.site.register(Message)
--- WeekChallenge/models.py
+++ WeekChallenge/models.py
@@ -21,3 +21,29 @@ class UserChallenge(models.Model):
 
     def __int__(self):
         return self.challenge_id
+
+
+class UserFriend(models.Model):
+    user_id = models.IntegerField(default=0)
+    friends = models.CommaSeparatedIntegerField(max_length=2000000000)
+
+    def __int__(self):
+        return self.user_id
+
+
+class Notification(models.Model):
+    user_id = models.IntegerField(default=0)
+    title = models.CharField(max_length=100000)
+    new = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+
+
+class Message(models.Model):
+    user_from = models.IntegerField(default=0)
+    user_to = models.IntegerField(default=0)
+    title = models.CharField(max_length=100000)
+    content = models.TextField()
+    date = models.DateTimeField('date sent')
+
